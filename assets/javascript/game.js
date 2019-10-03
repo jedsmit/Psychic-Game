025fb97100d262848a0ed50c686edbe0194a6929
Jed Smit <jedsmit1@gmail.com>
2019-10-02 22:13:12 -0700

    completed main game code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,38 +18,50 @@ function generateLetter(array) {
     return array[Math.floor(Math.random() * array.length)];
 };
 
-//----------------main game------------------------------------------
+//updates stats
 
-
-secretLetter = generateLetter(letters);
-console.log("secret: " + secretLetter);
-
-//get user's guess
-//runs 5 times
-//compare it to the secret letter
-// if true, increment wins, reset guesses left, so far, secret letter, and user guess
-// if false, decrement guesses left, add users guess to guesses so far 
-for (i = 0; i < 6; i++) {
-
+function updateStats() {
+    document.getElementById("wins").innerHTML = ("Wins: " + wins);
+    document.getElementById("losses").innerHTML = ("Losses: " + losses);
+    document.getElementById("guesses-left").innerHTML = ("Guesses left: " + guessesLeft);
+    document.getElementById("guesses-so-far").innerHTML = ("Guesses so far: " + guessesSoFar);
+};
+//----------------main game function start------------------------------------------
+function game() {
+    //get secret letter
+    secretLetter = generateLetter(letters);
+    console.log("secret: " + secretLetter);
+    //get user's guess
     document.onkeyup = function (event) {
         userGuess = event.key.toLowerCase();
         console.log("user: " + userGuess);
-
-
-        if (userGuess === secretLetter) {
+        //------------------compare guesses-------------------
+        //if not correct: decrement guesses left, update stats
+        if (userGuess != secretLetter) {
+            guessesLeft--;
+            guessesSoFar = guessesSoFar + " " + userGuess;
+            updateStats();
+            //if not correct *and* no guesses left: increment losses, reset guesses, update stats,     //restart game         
+            if (guessesLeft === 0) {
+                guessesLeft = 5;
+                guessesSoFar = "";
+                secretLetter = "";
+                losses++;
+                updateStats();
+                game();
+            };
+            //if correct: increment wins, reset guesses, update stats, restart game            
+        } else {
             wins++;
             guessesLeft = 5;
             guessesSoFar = "";
             secretLetter = "";
-        } else {
-            guessesLeft--;
-            guessesSoFar = guessesSoFar + " " + userGuess;
-            console.log(guessesSoFar);
+            updateStats();
+            game();
         };
 
-        document.getElementById("wins").innerHTML = ("Wins: " + wins);
-        document.getElementById("losses").innerHTML = ("Losses: " + losses);
-        document.getElementById("guesses-left").innerHTML = ("Guesses left: " + guessesLeft);
-        document.getElementById("guesses-so-far").innerHTML = ("Guesses so far: " + guessesSoFar);
     };
 };
+//----------------main game function end---------------
+// call game function
+game();
